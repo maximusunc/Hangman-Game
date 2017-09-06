8d86ff1fff4c100579f450ef9099aee1704212b2
Max Wang <maxwellswang@gmail.com>
2017-09-06 10:55:27 -0400

    added pseudocode to javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Set all variables to 0
+var wins = 0;
+var guessesRemaining = 10;
+var lettersGuessed = [];
+// Pick word to be guessed
+// Show blanks
+// 
+// Receive input from player
+// Check to see if letter is in word
+// If letter is in word, replace blank with the letter
+// If letter isn't in word, set number of guesses to -1
+// If letter has already been chosen, don't do anything
+// If number of guesses == 0, end the game and reset
+// If player solves the word, say congrats and reset
